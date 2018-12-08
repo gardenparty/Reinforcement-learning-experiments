a86ed7dafa38006185925207a22f304f4b073733
gardenparty <1430919768@qq.com>
2018-12-08 14:41:49 +0800

    add solution in annotate

--- DP/PolicyEvaluation.py
+++ DP/PolicyEvaluation.py
@@ -39,6 +39,21 @@ def policy_eval(policy, env, discount_factor=1.0, theta=0.00001):
         if delta < theta:
             break
         # TODO: Implement!
+           """
+           solution:
+           
+           delta=0
+           for s in range(env.nS):
+           v=0
+               for a,action_prob in enumerate(policy[s])
+                   for prob,next_state,reward,done in env.P[s][a]:
+                   v+=action_prob*prob*(reward+discount*V[next_state])
+               delta=max(delta,np.abs(v-V[s]))
+               v[s]=v
+           if delta<theta:
+               break
+            
+           """
     return np.array(V)
 random_policy = np.ones([env.nS, env.nA]) / env.nA
 v = policy_eval(random_policy, env)
